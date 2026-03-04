241e25d2f0dc481e82d9d0ab5d56d92fab7a44fb
hasnain257 <dastihasnain257@gmail.com>
2026-03-04 19:27:22 +0500

    updated

--- main.py
+++ main.py
@@ -116,14 +116,14 @@ c1, c2, c3, c4 = st.columns(4)
 c1.button("7", on_click=add_val, args=("7",))
 c2.button("8", on_click=add_val, args=("8",))
 c3.button("9", on_click=add_val, args=("9",))
-c4.button("÷", on_click=add_val, args=("/"))
+c4.button("➗", on_click=add_val, args=("/"))
 
 # Row 2
 c1, c2, c3, c4 = st.columns(4)
 c1.button("4", on_click=add_val, args=("4",))
 c2.button("5", on_click=add_val, args=("5",))
 c3.button("6", on_click=add_val, args=("6",))
-c4.button("×", on_click=add_val, args=("*"))
+c4.button("✖", on_click=add_val, args=("*"))
 
 # Row 3
 c1, c2, c3, c4 = st.columns(4)
